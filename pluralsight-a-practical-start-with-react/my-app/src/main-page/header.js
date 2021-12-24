042f8d49b59bb42d6a2a0b4454eb94c3bf4a431a
Kaya Koch <Kaya.Koch@gmail.com>
2021-12-24 12:01:53 +1100

    [PAPSWR] Added first React component, the Header

---
+++ pluralsight-a-practical-start-with-react/my-app/src/main-page/header.js
@@ -0,0 +1,15 @@
+
+const Header = () => (
+
+    <header className="row">
+        <div className="col-md-5">
+            <img src="/logo192.png" className="logo" alt="logo" />
+        </div>
+        <div className="col-md-7 mt-5 subtitle">
+            Providing widgets for everyone
+        </div>
+    </header>
+
+);
+
+export default Header;
