588002cd5f68dde217c50d9b2fe131dd27adc922
Kaya Koch <Kaya.Koch@gmail.com>
2022-01-07 17:09:03 +1100

    [PURH] Completed module 2

--- pluralsight-using-react-hooks/sample-app-next/pages/changescroll.js
+++ pluralsight-using-react-hooks/sample-app-next/pages/changescroll.js
@@ -1,12 +1,28 @@
 
+import { useState, useEffect } from "react";
 import ImageScroll from "../components/imagescroll";
 
 const ChangeScroll = ({offImg, onImg}) => {
+
+    const [currentId, setCurrentId] = useState(-1);
+    const [mouseEventCount, setMouseEventCount] = useState(0);
+
+    useEffect(() => {
+        document.title = `Viewing item ${currentId}`;
+    }, [currentId]);
+
     return (
         <div>
-            { Array.from({ length: 1 }, (_, i) =>
+            <p>Mouse Over Event Count: {mouseEventCount}</p>
+            { Array.from({ length: 10 }, (_, i) =>
                 (
-                    <div key={i}>
+                    <div
+                      key={i}
+                      onMouseOver={() => {
+                          setCurrentId(i);
+                          setMouseEventCount(mouseEventCount + 1);
+                      }}
+                    >
                         <ImageScroll offImg='off' onImg='on' />
                     </div>
                 )
